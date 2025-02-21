855bb63ce2ace0cf75ac7cc55cbb62403e8a8e1f
DylanDS25 <dylanleonardo11@gmail.com>
2025-02-21 17:19:09 -0500

    Agregar Servicio Biblioteca

---
+++ libreria/src/main/java/etitc/edu/co/sistemas/programacion/ServicioBiblioteca.java
@@ -0,0 +1,31 @@
+package etitc.edu.co.sistemas.programacion;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class ServicioBiblioteca {
+    Collection<Recurso> recurso = new ArrayList<>();
+
+    public void Biblioteca () {
+
+    }
+
+    public void agregar (Recurso recurso){
+
+    }
+
+    public void quitarRecurso (Recurso recurso){
+
+    }
+
+    public Collection<Recurso> buscarRecurso (String criterio){
+        Collection<Recurso> resultado = new ArrayList<>();
+        return resultado;
+    }
+
+    public Collection<Recurso> obtenerTodo() {
+        Collection<Recurso> resultado = new ArrayList<>();
+        return resultado;
+    }
+}
+
